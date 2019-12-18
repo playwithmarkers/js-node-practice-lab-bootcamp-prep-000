7f9a00a32023299929c0d36295cecebad7d6b404
Benjamin Connor <bsconnor@gmail.com>
2019-12-18 19:42:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // write your code below!
 
-var name = "Joe";
-var height = "74";
+var name = "Bilbo";
+var height = 48;
 
-/*console.log("Name:")
-console.log(name)
+console.log("Name:");
+console.log(name);
 
-console.log("Height:")
-console.log(height)
-*/
+console.log("Height:");
+console.log(height);
 
-module.exports = { name, height
+
+//module.exports = { name, height
 };
